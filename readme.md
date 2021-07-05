c8f05960f7fa4b8b84400eee6b0ff0bf7924971c
Max Erickson <maxerickson@gmail.com>
2021-07-05 15:51:56 -0400

    Update the overpass fetcher to take an input script, update round.py to take an input osm file.

--- readme.md
+++ readme.md
@@ -1,7 +1,13 @@
-Scripts to mechanically edit poorly formatted building heights from the [Austin, TX/Buildings Import](http://wiki.openstreetmap.org/wiki/Austin,_TX/Buildings_Import).
+Scripts to mechanically edit poorly formatted building heights from the 
+[Austin, TX/Buildings Import](http://wiki.openstreetmap.org/wiki/Austin,_TX/Buildings_Import).
 
-fetch.sh pulls data from Overpass-Api using this query: http://overpass-turbo.eu/s/ksJ and stores it in buildings.osm.
+fetch pulls data from Overpass-Api using a provided query file (following 
+this query: http://overpass-turbo.eu/s/ksJ for heights) and stores it in a
+.osm with the same base name as the query file.
 
-round.py reads buildings.osm and rounds the height values to 2 digits, saving the modified data in rounded_buildings.osm.
+round.py reads an input .osm file and rounds the height values to 2 digits, 
+saving the modified data in a file with the same base name and a .rounded.osm 
+extension.
 
-The changes in rounded_buildings.osm can be commited to OSM by opening the file in JOSM and uploading.
+The changes in the rounded.osm file can be commited to OSM by opening the file
+in JOSM and uploading.
--- round.py
+++ round.py
@@ -1,24 +1,33 @@
 #! /bin/python3
+import os.path
+import argparse
 import xml.etree.ElementTree as ElementTree
 
-infile="buildings.osm"
-outfile="rounded_buildings.osm"
-rounds=open("rounding.txt", "w")
+def round(infile, outfile, rounds):
+    tree=ElementTree.parse(infile)
+    osm=tree.getroot()
+    osm.set('generator', 'round.py')
+    for child in osm:
+        if child.tag in {"way","relation"}:
+            try:
+                # round the height.
+                t=child.findall("./tag[@k='height']")[0]
+                h=t.get('v')
+                newh='%.2f' % float(h)
+                rounds.write(h+' '+newh+'\n')
+                t.set('v', newh)
+                # mark element as modified.
+                child.set('action', 'modify')
+            except: # should only be child ways of relations
+                print(child.get('id'))
+    tree.write(outfile)
 
-tree=ElementTree.parse(infile)
-osm=tree.getroot()
-osm.set('generator', 'round.py')
-for child in osm:
-    if child.tag in {"way","relation"}:
-        try:
-            # round the height.
-            t=child.findall("./tag[@k='height']")[0]
-            h=t.get('v')
-            newh='%.2f' % float(h)
-            rounds.write(h+' '+newh+'\n')
-            t.set('v', newh)
-            # mark element as modified.
-            child.set('action', 'modify')
-        except: # should only be child ways of relations
-            print(child.get('id'))
-tree.write(outfile)
+if "__main__"==__name__:
+    parser = argparse.ArgumentParser(description='Trim excess digits from height values (uses Python float rounding rules).')
+    parser.add_argument('infile',
+                        help='Source data')
+    args = parser.parse_args()
+    fbase=os.path.splitext(os.path.basename(args.infile))[0]
+    outfile=fbase+'.rounded.osm'
+    rounds=open(fbase+'.rounds.txt', 'w')
+    round(args.infile, outfile, rounds)
